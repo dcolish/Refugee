7d1f6d0b8ceea3a9149582474e0bdd93d88e121f
Dan Colish <dcolish@gmail.com>
2011-06-08 19:32:39 -0700

    fill out migration api and complete simple cli interface

---
+++ refugee/cli.py
@@ -0,0 +1,65 @@
+from argparse import ArgumentParser
+from cmd import Cmd
+
+
+class RefugeeCmd(Cmd):
+    intro = 'Welcome to the Refugee Shell. Type help or ? to list commands\n'
+    prompt = 'refugee> '
+
+    def do_down(self, arg):
+        """Run down migration with name or numeric id matching arg"""
+        print "running down migration"
+
+    def do_init(self, arg):
+        """Run down migration with name or numeric id matching arg"""
+        print "initializing migrations"
+
+    def do_migrate(self, arg):
+        """Run all up migrations"""
+        print "running migrations"
+
+    def do_new(self, arg):
+        """Run down migration with name or numeric id matching arg"""
+        print "initializing migrations"
+
+    def do_up(self, arg):
+        """Run up migration with name or numeric id matching arg"""
+        print "running up migration"
+
+    def do_exit(self, arg):
+        """Quit the interactive shell"""
+        exit()
+
+    def do_quit(self, arg):
+        """Quit the interactive shell"""
+        exit()
+
+    def default(self, line):
+        if line == 'EOF':
+            print
+            exit()
+        else:
+            print "Unrecognizable command"
+            self.do_help('')
+
+    def emptyline(self):
+        # Do nothing with empty lines
+        pass
+
+
+if __name__ == "__main__":
+    cli = RefugeeCmd()
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--config', help='Configuration File')
+    parser.add_argument('command', metavar='CMD', type=str, nargs='?',
+                        default='', help="command")
+    parser.add_argument('parameters', metavar='ARG', type=str, nargs="*",
+                        help="command parameters")
+
+    options = parser.parse_args()
+    command = options.command
+    parameters = ' '.join(options.parameters)
+    if command == '' and parameters == '':
+        cli.cmdloop()
+    else:
+        cli.onecmd(' '.join((command, parameters)))
--- refugee/migration.py
+++ refugee/migration.py
@@ -2,15 +2,24 @@
 Here we define the API for migrations.
 
 Classically, a migration has two methods which a user may call, up and
-down. These will be retained.
-
+down. These will be retained. In addition, there are two optional methods which
+will get run on all migrations, check and preflight. These must be overridden in
+a subclass to have any effect
 """
 
 
 class Migration(object):
 
-    def up(self, connection):
-        raise NotImplementedError
+    def check(self, connection):
+        """Ensures that a given migration ran successfully"""
+        return True
 
     def down(self, connection):
+        raise NotImplementedError
+
+    def preflight(self, connection):
+        """Operations to run before `self.up` is run"""
+        return
+
+    def up(self, connection):
         raise NotImplementedError
